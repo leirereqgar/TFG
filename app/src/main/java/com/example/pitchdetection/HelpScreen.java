0d0f3f43470e0f6a2832f9fe89529c465d1ddaa3
leirereqgar <leirereqgar@gmail.com>
2023-03-23 11:41:33 +0100

    HelpScreen añadida

---
+++ app/src/main/java/com/example/pitchdetection/HelpScreen.java
@@ -0,0 +1,13 @@
+package com.example.pitchdetection;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelpScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_screen);
+    }
+}
