b4a8137d02eeb2603bbe6f6606e163ab1cfbedee
leirereqgar <leirereqgar@gmail.com>
2023-03-23 11:30:10 +0100

    HomScreen creada

---
+++ app/src/main/java/com/example/pitchdetection/HomeScreen.java
@@ -0,0 +1,80 @@
+package com.example.pitchdetection;
+
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.RECORD_AUDIO;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class HomeScreen extends AppCompatActivity {
+    private int request_perm_code;
+    Button start_btn, help_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+        /* Si no se tienen permisos se piden, y si sigen sin estar antes de cerrar la aplicacion
+         * se muestra un aviso
+         */
+        if(!checkPermissions()) {
+            requestPermissions();
+            if(!checkPermissions()) {
+                Toast.makeText(this, "Son necesarios los permisos de camara y microfono",
+                        Toast.LENGTH_SHORT);
+            }
+        }
+
+        start_btn = findViewById(R.id.start);
+        start_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToLessons();
+            }
+        });
+
+        help_btn  = findViewById(R.id.help);
+        help_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                help();
+            }
+        });
+    }
+
+    /*
+    * En las ultimas versiones de android es necesario pedir de forma explicita los permisos
+    * en este caso son necesarios los permisos de acceso a la camara y al microfono
+    */
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(HomeScreen.this,
+                new String[]{CAMERA, RECORD_AUDIO}, request_perm_code);
+    }
+
+    public boolean checkPermissions() {
+        return (ContextCompat.checkSelfPermission(getApplicationContext(), CAMERA)
+                == PackageManager.PERMISSION_GRANTED)
+                &&
+                (ContextCompat.checkSelfPermission(getApplicationContext(), RECORD_AUDIO)
+                        == PackageManager.PERMISSION_GRANTED);
+    }
+
+    private void goToLessons() {
+        Intent change = new Intent(this, LessonsScreen.class);
+        startActivity(change);
+    }
+
+    private void help() {
+        //TODO: crear la pantalla de ayuda y hacer que el cambio funcione
+        Toast.makeText(this, "TODO", Toast.LENGTH_SHORT);
+    }
+}
