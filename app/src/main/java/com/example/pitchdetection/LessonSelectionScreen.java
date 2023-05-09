41280e7a5753d12b54375360e8be2d9c4cb68dc5
leirereqgar <leirereqgar@gmail.com>
2023-05-09 20:50:11 +0200

    El carrusel avanza pero hace cosas raras al ir hacia atrás

--- app/src/main/java/com/example/pitchdetection/LessonSelectionScreen.java
+++ app/src/main/java/com/example/pitchdetection/LessonSelectionScreen.java
@@ -2,35 +2,67 @@ package com.example.pitchdetection;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.helper.widget.Carousel;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class LessonSelectionScreen extends AppCompatActivity {
-    private static int n_lessons = 4;
-    private ArrayList<Button> buttons;
+    private static int n_lessons;
+    private ArrayList<TextView> buttons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lessons_selection_screen);
 
-        setUpCarousel();
-
+        
         buttons = new ArrayList<>();
         buttons.add(findViewById(R.id.c1));
-        for (int i = 0; i < buttons.size(); i++) {
-            buttons.get(i).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    startLesson("Major");
-                }
-            });
-        }
+        buttons.get(0).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startLesson("Major");
+            }
+        });
+
+        buttons.add(findViewById(R.id.c2));
+        buttons.get(1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startLesson("Minor");
+            }
+        });
+
+        buttons.add(findViewById(R.id.c3));
+        buttons.get(2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startLesson("Major");
+            }
+        });
+
+        buttons.add(findViewById(R.id.c4));
+        buttons.get(3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startLesson("Major");
+            }
+        });
+
+        n_lessons = 4;
+
+        setUpCarousel();
+
+
     }
 
     private void startLesson(String extra) {
@@ -49,7 +81,7 @@ public class LessonSelectionScreen extends AppCompatActivity {
 
             @Override
             public void populate(View view, int index) {
-
+                ((TextView)view).setText(buttons.get(index).getText());
             }
 
             @Override
