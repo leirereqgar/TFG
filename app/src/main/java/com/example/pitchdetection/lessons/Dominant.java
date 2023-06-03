292dcf16bc268a2b8698eebc8dc1f05b80ff2613
leirereqgar <leirereqgar@gmail.com>
2023-06-03 21:32:34 +0200

    Lecciones nuevas

---
+++ app/src/main/java/com/example/pitchdetection/lessons/Dominant.java
@@ -0,0 +1,57 @@
+package com.example.pitchdetection.lessons;
+
+import com.example.pitchdetection.enums.ChordTypeEnum;
+import com.example.pitchdetection.enums.NoteNameEnum;
+
+import java.util.ArrayList;
+
+public class Dominant extends Lesson{
+    @Override
+    public void init() {
+        code = ChordTypeEnum.Dominant;
+
+        chords = new ArrayList<>();
+
+        Chord c_dominant = new Chord(NoteNameEnum.C, ChordTypeEnum.Dominant);
+        c_dominant.add(new Note(2,1));
+        c_dominant.add(new Note(4,2));
+        c_dominant.add(new Note(5,3));
+        c_dominant.add(new Note(3,3));
+        chords.add(c_dominant);
+
+        Chord d_dominant = new Chord(NoteNameEnum.D, ChordTypeEnum.Dominant);
+        d_dominant.add(new Note(2,1));
+        d_dominant.add(new Note(3,2));
+        d_dominant.add(new Note(1,2));
+        chords.add(d_dominant);
+
+        Chord e_dominant = new Chord(NoteNameEnum.E, ChordTypeEnum.Dominant);
+        e_dominant.add(new Note(3,1));
+        e_dominant.add(new Note(5,2));
+        chords.add(e_dominant);
+
+        Chord f_dominant = new Chord(NoteNameEnum.F, ChordTypeEnum.Dominant);
+        f_dominant.add(new Note(0,1));
+        f_dominant.add(new Note(3,2));
+        f_dominant.add(new Note(5,3));
+        chords.add(f_dominant);
+
+        Chord g_dominant = new Chord(NoteNameEnum.G, ChordTypeEnum.Dominant);
+        g_dominant.add(new Note(1,1));
+        g_dominant.add(new Note(5,2));
+        g_dominant.add(new Note(6,3));
+        chords.add(g_dominant);
+
+        Chord a_dominant = new Chord(NoteNameEnum.A, ChordTypeEnum.Dominant);
+        a_dominant.add(new Note(4,2));
+        a_dominant.add(new Note(2,2));
+        chords.add(a_dominant);
+
+        Chord b_dominant = new Chord(NoteNameEnum.B, ChordTypeEnum.Dominant);
+        b_dominant.add(new Note(4,1));
+        b_dominant.add(new Note(5,2));
+        b_dominant.add(new Note(3,2));
+        b_dominant.add(new Note(1,2));
+        chords.add(b_dominant);
+    }
+}
---
+++ app/src/main/java/com/example/pitchdetection/lessons/Suspended.java
@@ -0,0 +1,53 @@
+package com.example.pitchdetection.lessons;
+
+public class Suspended extends Lesson{
+    @Override
+    public void init() {
+        code = ChordTypeEnum.Suspended;
+
+        chords = new ArrayList<>();
+
+        Chord c_suspended = new Chord(NoteNameEnum.C, ChordTypeEnum.Suspended);
+        c_suspended.add(new Note(2,1));
+        c_suspended.add(new Note(5,3));
+        c_suspended.add(new Note(4,3));
+        c_suspended.add(new Note(1,3));
+        chords.add(c_suspended);
+
+        Chord d_suspended = new Chord(NoteNameEnum.D, ChordTypeEnum.Suspended);
+        d_suspended.add(new Note(3,2));
+        d_suspended.add(new Note(2,3));
+        d_suspended.add(new Note(1,3));
+        chords.add(d_suspended);
+
+        Chord e_suspended = new Chord(NoteNameEnum.E, ChordTypeEnum.Suspended);
+        e_suspended.add(new Note(5,2));
+        e_suspended.add(new Note(4,2));
+        e_suspended.add(new Note(3,2));
+        chords.add(e_suspended);
+
+        Chord f_suspended = new Chord(NoteNameEnum.F, ChordTypeEnum.Suspended);
+        f_suspended.add(new Note(0,1));
+        f_suspended.add(new Note(5,3));
+        f_suspended.add(new Note(4,3));
+        f_suspended.add(new Note(3,3));
+        chords.add(f_suspended);
+
+        Chord g_suspended = new Chord(NoteNameEnum.G, ChordTypeEnum.Suspended);
+        g_suspended.add(new Note(6,3));
+        g_suspended.add(new Note(2,3));
+        g_suspended.add(new Note(1,3));
+        chords.add(g_suspended);
+
+        Chord a_suspended = new Chord(NoteNameEnum.A, ChordTypeEnum.Suspended);
+        a_suspended.add(new Note(4,2));
+        a_suspended.add(new Note(3,2));
+        chords.add(a_suspended);
+
+        Chord b_suspended = new Chord(NoteNameEnum.B, ChordTypeEnum.Suspended);
+        b_suspended.add(new Note(0,2));
+        b_suspended.add(new Note(4,4));
+        b_suspended.add(new Note(3,4));
+        chords.add(b_suspended);
+    }
+}
