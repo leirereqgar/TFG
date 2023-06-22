c8ac319595b942f27ca15ab8c0490728be0bba6f
leirereqgar <leirereqgar@gmail.com>
2023-06-22 18:49:46 +0200

    Cambiada referencia a paquete de java·

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -4,7 +4,7 @@
 extern "C"{
     ChordProcessor * cd = new ChordProcessor();
 
-    jintArray Java_com_example_pitchdetection_services_ChordRecognitionService_chordDetection(JNIEnv *env, jclass clazz, jdoubleArray samples, jdoubleArray spectrum_samples) {
+    jintArray Java_com_example_GuitAR_services_ChordRecognitionService_chordDetection(JNIEnv *env, jclass clazz, jdoubleArray samples, jdoubleArray spectrum_samples) {
         jintArray output = env->NewIntArray(2);
 
         env->SetIntArrayRegion(output,0,2, cd->chordDetection(env->GetDoubleArrayElements(samples,0), env->GetDoubleArrayElements(spectrum_samples,0)));
@@ -12,13 +12,13 @@ extern "C"{
         return output;
     }
 
-    jdoubleArray Java_com_example_pitchdetection_services_ChordRecognitionService_getChromagram(JNIEnv *env, jclass clazz) {
+    jdoubleArray Java_com_example_GuitAR_services_ChordRecognitionService_getChromagram(JNIEnv *env, jclass clazz) {
         jdoubleArray output = env->NewDoubleArray(12);
         env->SetDoubleArrayRegion(output,0,12,cd->getChromagram());
         return output;
     }
 
-    jdoubleArray Java_com_example_pitchdetection_services_ChordRecognitionService_window(JNIEnv *env, jclass clazz, jdoubleArray samples) {
+    jdoubleArray Java_com_example_GuitAR_services_ChordRecognitionService_window(JNIEnv *env, jclass clazz, jdoubleArray samples) {
         jsize length = env->GetArrayLength(samples);
         jdoubleArray output = env->NewDoubleArray(length);
 
@@ -27,7 +27,7 @@ extern "C"{
         return output;
     }
 
-    jdoubleArray Java_com_example_pitchdetection_services_ChordRecognitionService_fft(JNIEnv *env, jclass clazz, jdoubleArray inputReal, jboolean DIRECT) {
+    jdoubleArray Java_com_example_GuitAR_services_ChordRecognitionService_fft(JNIEnv *env, jclass clazz, jdoubleArray inputReal, jboolean DIRECT) {
         jsize length = env->GetArrayLength(inputReal);
         double* samplesDouble = ChordProcessor::fft(env->GetDoubleArrayElements(inputReal, 0), length, DIRECT);
         jdoubleArray output = env->NewDoubleArray(length);
@@ -36,7 +36,7 @@ extern "C"{
         return output;
     }
 
-    jdoubleArray Java_com_example_pitchdetection_services_ChordRecognitionService_bandPassFilter(JNIEnv *env, jclass clazz,
+    jdoubleArray Java_com_example_GuitAR_services_ChordRecognitionService_bandPassFilter(JNIEnv *env, jclass clazz,
                                                                             jdoubleArray samples, jfloat low_cut,
                                                                             jfloat high_cut, jfloat sr,
                                                                             jfloat buffer_size) {
